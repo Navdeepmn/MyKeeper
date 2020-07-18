0b56ae100e1893e9b495ca43563070b00a7b5ecb
Navdeep Mandal <navdeep.mandal2@gmail.com>
2020-07-19 01:46:46 +0530

    Updates

---
+++ precache-manifest.d5637e0e970337d1144f2e85985b5f97.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebae5845f301e3b10505a858b6ca71d6",
+    "url": "/MyKeeper/index.html"
+  },
+  {
+    "revision": "c4a8c31bb4d0a373dae9",
+    "url": "/MyKeeper/static/js/2.ce917ca5.chunk.js"
+  },
+  {
+    "revision": "6cfaff9748f200eecde2d0a49d419313",
+    "url": "/MyKeeper/static/js/2.ce917ca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9318d4114e0e263be74a",
+    "url": "/MyKeeper/static/js/main.b092673d.chunk.js"
+  },
+  {
+    "revision": "052bbc5353a1204c7971",
+    "url": "/MyKeeper/static/js/runtime-main.8f44d4c7.js"
+  }
+]);
