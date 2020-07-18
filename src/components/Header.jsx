565315798ca34ee5b27fae051ed486bfe69d1e11
Navdeep Mandal <navdeep.mandal2@gmail.com>
2020-07-19 01:54:39 +0530

    Create this React app and publish it to GitHub Pages

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Face from "@material-ui/icons/Face"
+
+
+function Header() {
+    return <header>
+      <h1><Face /> My-Keeper</h1>
+    </header>
+}
+ 
+export default Header;
